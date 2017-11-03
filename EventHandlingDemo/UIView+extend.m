e30469eb34aa430a3b79b124ae1a34da50cbb7cb
张诗健 <zsjcwy_xcode@163.com>
2017-11-03 15:12:43 +0800

    add

---
+++ EventHandlingDemo/UIView+extend.m
@@ -0,0 +1,39 @@
+//
+//  UIView+extend.m
+//  EventHandlingDemo
+//
+//  Created by 讯心科技 on 2017/11/3.
+//  Copyright © 2017年 讯心科技. All rights reserved.
+//
+
+#import "UIView+extend.h"
+#import <objc/runtime.h>
+
+@implementation UIView (extend)
+
++ (void)load
+{
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        
+        SEL originalSelector = @selector(hitTest:withEvent:);
+        
+        SEL currentSelector = NSSelectorFromString(@"z_hitTest:withEvent:");
+        
+        Method original = class_getInstanceMethod([self class], originalSelector);
+        
+        Method current  = class_getInstanceMethod([self class], currentSelector);
+        
+        method_exchangeImplementations(original, current);
+    });
+}
+
+- (UIView *)z_hitTest:(CGPoint)point withEvent:(UIEvent *)event
+{
+    NSLog(@"%@",NSStringFromClass(self.class));
+    
+    return [self z_hitTest:point withEvent:event];
+}
+
+@end
